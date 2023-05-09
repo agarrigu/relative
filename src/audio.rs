67a8df2b89d357dc63144f03fda49034c4119dfa
agarrigu <agarrigu@protonmail.com>
2023-05-09 17:28:50 +0100

    Add gen_tri_wave to src/audio.rs

--- src/audio.rs
+++ src/audio.rs
@@ -1,14 +1,14 @@
 use std::mem::size_of;
 use std::f64::consts::PI;
 
-const FRAME_RATE:       u32 = 60;
-pub const SAMPLE_RATE:      i32 = 44_100;
-pub const TABLE_SIZE:     usize = SAMPLE_RATE as usize;
-pub const VOLUME:           f32 = 0.07;
-pub const WAVE_SIZE:      usize = size_of::<f32>()
-                              * CHANNELS as usize
-                              * (TABLE_SIZE / FRAME_RATE as usize);
-pub const CHANNELS:          u8 = 2;
+const FRAME_RATE:      u32   = 60;
+pub const SAMPLE_RATE: i32   = 48_000;
+pub const TABLE_SIZE:  usize = SAMPLE_RATE as usize;
+pub const VOLUME:      f32   = 0.07;
+pub const CHANNELS:    u8    = 2;
+pub const WAVE_SIZE:   usize = size_of::<f32>()
+                               * CHANNELS as usize
+                               * (TABLE_SIZE / FRAME_RATE as usize);
 
 #[derive(Copy, Clone)]
 pub struct Phase {
@@ -24,7 +24,7 @@ impl Phase {
         }
     }
 
-    pub fn incr(&mut self, freq: f32) {
+    pub fn next_ampl(&mut self, freq: f32) {
         let table_size = TABLE_SIZE as f32;
         self.left += freq;
         self.right += freq;
@@ -32,7 +32,6 @@ impl Phase {
         if self.right >= table_size {self.right -= table_size}
     }
 }
-
 
 pub fn gen_sqr_wave() -> [f32; TABLE_SIZE] {
     let mut square_wave = [VOLUME; TABLE_SIZE];
@@ -48,4 +47,16 @@ pub fn gen_sine_wave() -> [f32; TABLE_SIZE] {
         sine_wave[i] = (i as f64 / TABLE_SIZE as f64 * PI * 2.0).sin() as f32 * VOLUME;
         i += 1;
     } sine_wave
+}
+
+pub fn gen_tri_wave() -> [f32; TABLE_SIZE] {
+    let mut triangle_wave = [0.0; TABLE_SIZE];
+    let mut i = 0;
+    let mut amplitude = -1f32;
+    let step = 2f32 / TABLE_SIZE as f32;
+    while i < TABLE_SIZE {
+        triangle_wave[i] = amplitude;
+        amplitude += step;
+        i += 1;
+    } triangle_wave
 }
